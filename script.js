772a478e52dad470be72f8cafae6449cc3319ccf
xand144 <174009470+xand144@users.noreply.github.com>
2026-03-17 11:41:21 +0000

    Comment code

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+// Computer randomly chooses "Rock", "Paper" or "Scissors"
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
 
+    // Convert number into corresponding choice
     switch (randNum) {
         case 0:
             return "Rock";
@@ -11,17 +14,24 @@ function getComputerChoice() {
         case 2:
             return "Scissors";
             break;
+        // Silly error message; should never run if the code is written correctly
         default:
             console.log("oopsie woopsie! we made a fucky wucky!");
     }
 }
 
+// Get the player to choose rock, paper or scissors
 function getHumanChoice() {
+    // Get player input and store in variable
     let choice = prompt("Rock, Paper, or Scissors?");
+
+    // Capitalises first letter of player input and makes the rest lower case
     let normaliseCase = () => {
         choice = choice.charAt(0).toUpperCase() 
         + choice.slice(1).toLowerCase();
     }
+
+    // Runs if no response given
     let noResponse = () => {
         alert("You failed to provide a response. You do know how Rock Paper Scissors works right?");
         choice = prompt("Invalid. Enter \"Rock\", \"Paper\", or \"Scissors\"");
@@ -34,6 +44,8 @@ function getHumanChoice() {
                 invalidResponse()
         }
     }
+
+    // Runs if response is neither "Rock", "Paper" nor "Scissors"
     let invalidResponse = () => {
         choice = prompt("Invalid. Enter \"Rock\", \"Paper\", or \"Scissors\"");
         normaliseCase();
@@ -45,8 +57,10 @@ function getHumanChoice() {
             invalidResponse();
         }
     }
+
     normaliseCase()
     
+    // Reprompts if response absent or invalid
     if (choice == false) {
         noResponse();
     } else if (choice !== "Rock" 
@@ -55,12 +69,14 @@ function getHumanChoice() {
             invalidResponse();
     }
 
+    // Return the player's choice, when it is one of "Rock" "Paper" or "Scissors"
     return choice;
 }
 
+// Store computer choice in variable
 let computerChoice;
 computerChoice = getComputerChoice()
-
+// Store player's choice in variable
 let humanChoice;
 humanChoice = getHumanChoice();
 console.log(humanChoice);
