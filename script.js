78552be207b48b58df5e20479da972f1c94ace6a
xand144 <174009470+xand144@users.noreply.github.com>
2026-03-17 09:36:17 +0000

    Create function to return rock paper or scissors
    * Returns rock, paper, or scissors, depending on the random
      number from 0-2 generated
    * Create Variable to store the computer choice

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
+    switch (randNum) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+        default:
+            console.log("oopsie woopsie! we made a fucky wucky!");
+    }
 }
 
-getComputerChoice()
+let computerChoice;
+computerChoice = getComputerChoice()
